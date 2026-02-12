2082cd5693567fb7459948d3a4660024e2ef3ad1
Antoine Audrain <me@antoineaudrain.com>
2026-02-12 21:12:57 +0100

    Bump version to 1.0.6 and update @siteline/core dependency and related references.

--- test/tracking.spec.ts
+++ test/tracking.spec.ts
@@ -57,7 +57,7 @@ describe('track', () => {
 			expect(Siteline).toHaveBeenCalledWith({
 				websiteKey: mockEnv.SITELINE_WEBSITE_KEY,
 				sdk: '@siteline/cloudflare-worker',
-				sdkVersion: '1.0.2',
+				sdkVersion: '1.0.6',
 				integrationType: 'cloudflare-worker',
 			});
 		});
